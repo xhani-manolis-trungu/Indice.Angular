238afed7b516faeb2c77b4e27a12466c6ddee011
NCLAN\xtrungu <xtrungu@netcompany.com>
2025-02-04 15:32:02 +0200

    add documentation for exported Tokens

--- projects/ng-config/src/lib/tokens.ts
+++ projects/ng-config/src/lib/tokens.ts
@@ -3,6 +3,18 @@ import { IAppSettings, AppProvidersArray, IAuthSettings } from "./types";
 
 export const APP_SETTINGS = new InjectionToken<IAppSettings>('APP_SETTINGS');
 export const IAUTH_SETTINGS = new InjectionToken<IAuthSettings>('IAUTH_SETTINGS');
+/**
+ * Utilize this token to provide the initial structure of your environment in the consuming application
+ * 
+ * Usage: { provide: APP_ENVIRONMENT, useValue: environment },
+ */
 export const APP_ENVIRONMENT = new InjectionToken<any>('APP_ENVIRONMENT');
+/**
+ * Utilize this to provide the array of providers
+ * by calculating on runtime the value of the token key from APP_SETTINGS after they are set by the Initializer
+ */
 export const APP_PROVIDERS_ARRAY = new InjectionToken<AppProvidersArray>('APP_PROVIDERS_ARRAY');
+/**
+ * Override settings URL on the consuming application
+ */
 export const APP_SETTINGS_MANIFEST_URL = new InjectionToken<string>('APP_SETTINGS_MANIFEST_URL');
